e3992fdb36334264446e3a5b387bc1a85f750d28
Allan Estrada <allan.prodigious@digitalbi.com>
2020-02-19 12:54:38 -0600

    Th last commit of styles

--- src/app/components/shared/slider/slider.component.ts
+++ src/app/components/shared/slider/slider.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  selector: "app-slider",
+  templateUrl: "./slider.component.html",
+  styleUrls: ["./slider.component.scss"]
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  id: string = '';
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  scrollTo( id ) {
+    console.log(id);
+
+    const elmnt = document.getElementById( id );
+    elmnt.scrollIntoView({ block: 'start', behavior: 'smooth' });
+
   }
-
 }
--- src/app/services/wr.service.ts
+++ src/app/services/wr.service.ts
@@ -7,34 +7,34 @@ export class WrService {
 
   private modalImages = {
     "ATL": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "Trademarketing": {
-      "photo": "assets/images/btl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "BTL": {
-      "photo": "assets/images/btl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "POP y Display": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "Social Media": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "MKT Campaign": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "Retail": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "RRHH Outsorsing": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "Gráfico, Digital & Video": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     },
     "Business Inteligence": {
-      "photo": "assets/images/atl.jpg"
+      "photo": "assets/images/social-media.jpg"
     }
   };
 
